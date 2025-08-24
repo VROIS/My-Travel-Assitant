f64462f31e5cb6558915fe582b77c5c1c0c99601
VROIS <dbstour1@gmail.com>
2025-08-24 03:53:58 +0200

    feat: Improve UI consistency and security
    Refactors the main page display logic and strengthens API security by addressing potential key exposure. Updates UI elements for better readability and applies a new font for a more refined aesthetic.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-travel-assistant-cache-v9'; // Force update by bumping cache version
+const CACHE_NAME = 'my-travel-assistant-cache-v15'; // Force update by bumping cache version
 const urlsToCache = [
   '/',
   'index.html',
